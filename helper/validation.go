f3b81414f2c51f92ca13d03b1d44ac73ee2cd5b7
adimasmudi <adimasmudi@gmail.com>
2023-06-07 20:15:25 +0700

    add auth middleware

--- helper/validation.go
+++ helper/validation.go
@@ -1,6 +1,12 @@
 package helper
 
-import "github.com/go-playground/validator/v10"
+import (
+	"errors"
+	"os"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt/v4"
+)
 
 func FormatValidationError(err error) []string {
 	var errors []string
@@ -10,4 +16,20 @@ func FormatValidationError(err error) []string {
 	}
 
 	return errors
+}
+
+func ValidateToken(encodedToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid token")
+		}
+
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
---
+++ middlewares/auth.go
@@ -0,0 +1,53 @@
+package middlewares
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+	"pronics-api/helper"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func Auth(c *fiber.Ctx)error{
+	
+	authorizationHeader := c.Get("Authorization")
+	
+	if !strings.Contains(authorizationHeader, "Bearer"){
+		response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", errors.New("you have to use bearer"))
+		c.Status(http.StatusUnauthorized).JSON(response)
+		return nil
+	}
+
+	tokenArray := strings.Split(authorizationHeader," ")
+	if len(tokenArray) < 2{
+		response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", errors.New("can't Find token"))
+		c.Status(http.StatusUnauthorized).JSON(response)
+		return nil
+	}
+
+	tokenString := tokenArray[1]
+
+	token, err := helper.ValidateToken(tokenString)
+
+	if err != nil{
+		response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", errors.New("token is not valid"))
+		c.Status(http.StatusUnauthorized).JSON(response)
+		return err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok || !token.Valid{
+		response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", errors.New("token is not valid"))
+		c.Status(http.StatusUnauthorized).JSON(response)
+		return nil
+	}
+
+	email := claims["email"].(string)
+	
+	c.Locals("currentUserEmail",email)
+
+	return c.Next()
+}
