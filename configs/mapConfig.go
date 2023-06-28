a91a9cf7d8a606b8a16375a7b5cad50f4cd355ed
adimasmudi <adimasmudi@gmail.com>
2023-06-28 10:48:16 +0700

    apply no exposed key

--- configs/mapConfig.go
+++ configs/mapConfig.go
@@ -2,12 +2,13 @@ package configs
 
 import (
 	"log"
+	"os"
 
 	"googlemaps.github.io/maps"
 )
 
 func InitMap() *maps.Client {
-	c, err := maps.NewClient(maps.WithAPIKey("AIzaSyB8Xcw0-bTqcs2vXOQ5SANu65-4IR1rRFc"))
+	c, err := maps.NewClient(maps.WithAPIKey(os.Getenv("MAPS_API_KEY")))
 
 	if err != nil {
 		log.Fatalf("fatal error: %s", err)
